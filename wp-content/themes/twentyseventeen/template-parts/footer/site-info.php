cfb3ae3212782ef35fe859e1cde0f810f2e8a730
Vineet goel <vineetgoyal009@hotmail.com>
2017-02-01 12:13:33 +0530

    Update site-info.php

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Project by %s', 'twentyseventeen' ), 'Vineet' ); ?></a>
 </div><!-- .site-info -->
