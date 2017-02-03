24e55cbcaba2387bea338ab846cea76b7bfd9ab1
nicbastienne <nbastienne1@gmail.com>
2017-02-03 13:59:34 +0000

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -95,6 +95,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
+define('WP_ALLOW_REPAIR', true);
 define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
