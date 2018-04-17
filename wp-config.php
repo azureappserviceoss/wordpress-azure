9594f9f546b32a03a23c9aacb24f7ec017df58be
Sunitha Muthukrishna <mksunitha@gmail.com>
2018-04-17 14:19:44 -0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -27,29 +27,12 @@ $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
-if (isset($_SERVER['MYSQLCONNSTR_localdb']))
-{
-//Windows app service
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-   }
-   
-   
-}
-else
-{
+
     $connectstr_dbhost = getenv('DATABASE_HOST');
     $connectstr_dbname = getenv('DATABASE_NAME');;
     $connectstr_dbusername = getenv('DATABASE_USERNAME');;
     $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
-}
+
 
 define('DB_NAME', getenv('DATABASE_NAME'));
 
