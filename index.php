417d31cc3b4369ac671948e7badcd1b2a78dd7ee
max-ma1 <max.ma@live.ca>
2018-07-26 15:28:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+"<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -15,4 +15,4 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
-echo 'hello';
+echo "hello";
