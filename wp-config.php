0f5c422b81068be5bc11188ed296b34309926314
VirdisD <54962892+VirdisD@users.noreply.github.com>
2020-04-08 16:43:24 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -105,7 +105,9 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+
 
 /* That's all, stop editing! Happy blogging. */
 
